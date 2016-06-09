95684761ecb3560b7e902ab9a13c872600633f3c
AJ Grande <ajgrande924@gmail.com>
2016-06-09 15:33:47 -0700

    fixed styling

--- roam/App/Components/PickActivity.js
+++ roam/App/Components/PickActivity.js
@@ -2,12 +2,17 @@
 
 import React, { Component } from 'react';
 import {
+    Dimensions,
     StyleSheet,
     Text,
     View,
+    TouchableHighlight
 } from 'react-native';
 
 import FMPicker from '../Lib/index.js';
+
+var deviceWidth = Dimensions.get('window').width;
+var deviceHeight = Dimensions.get('window').height;
 
 var options = ['Hot Yoga', 'Regular Yoga', 'Restaurants', 'Bars', 'Food', 'Excercise', 'Siteseeing', ];
 
@@ -22,19 +27,21 @@ class ActivityPicker extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>{this.state.selectedOption}</Text>
-                <Text
-                    style={{color:'blue', marginTop: 20}}
-                    onPress={()=>{
-                        this.refs.picker.show();
-                    }}>
-                    Click here to select your option
-                </Text>
-                <FMPicker ref={'picker'} options={options}
-                    onSubmit={(option)=>{
-                        this.setState({selectedOption: option})
-                    }}
-                    />
+                <View style={styles.textContainer}>
+                <Text style={styles.title}> I want to go to {this.state.selectedOption}!</Text>
+                </View>
+                <View style={styles.buttonContainer}>
+                    <TouchableHighlight
+                      style={styles.button}
+                      onPress={() => { this.refs.picker.show() }} >
+                        <Text style={styles.buttonText}>Select</Text>
+                    </TouchableHighlight>
+                    <FMPicker ref={'picker'} options={options}
+                        onSubmit={(option)=>{
+                            this.setState({selectedOption: option})
+                        }}
+                        />
+                </View>
             </View>
         );
     }
@@ -45,7 +52,18 @@ var styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#F5FCFF',
+        backgroundColor: 'transparent',
+        flexDirection: 'row'
+    },
+    textContainer: {
+        width: 3 * deviceWidth/4
+    },
+    buttonContainer: {
+        width: deviceWidth/4
+    },
+    title: {
+        fontSize: 18,
+        color: 'white',
     },
     welcome: {
         fontSize: 20,
@@ -56,6 +74,21 @@ var styles = StyleSheet.create({
         textAlign: 'center',
         color: '#333333',
         marginBottom: 5,
+    },
+    buttonText: {
+      fontSize: 18,
+      color: 'white',
+      alignSelf: 'center'
+    },
+    button: {
+      height: deviceHeight/15,
+      width: deviceWidth/4,
+      flexDirection: 'row',
+      backgroundColor: '#ff0066',
+      // borderRadius:10,
+      marginBottom: 10,
+      alignSelf: 'center',
+      justifyContent: 'center',
     },
 });
 
