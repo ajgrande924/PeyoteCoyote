c796bad55a3e89242dc982c019643092399e6c50
AJ Grande <ajgrande924@gmail.com>
2016-06-11 01:43:14 -0700

    designed user view

--- roam/App/Components/User.js
+++ roam/App/Components/User.js
@@ -108,7 +108,7 @@ class User extends Component {
     return (
       <View>
         <Image style={styles.backgroundImage}
-        source={require('../../imgs/uni.jpg')}>
+        source={require('../../imgs/bridge.png')}>
         <View style={styles.navbarContainer}>
           <View style={styles.profileContainer}>
             <View>
@@ -224,10 +224,11 @@ class GridListItem extends Component {
 const styles = StyleSheet.create({
   navbarContainer:{
     backgroundColor: 'transparent',
+    marginTop: deviceHeight/3.05,
     paddingTop: deviceHeight/25,
     height: deviceHeight/3,
-    borderBottomColor: 'white',
-    borderWidth: 0.5
+    // borderBottomColor: 'white',
+    // borderWidth: 0.5
   },
   navTitle: {
     color:'#fff',
@@ -265,12 +266,14 @@ const styles = StyleSheet.create({
   },
   statTitle: {
     fontSize: 12,
-    color: '#ff0066',
+    // color: '#ff0066',
+    color: 'white',
   },
   statTitle2: {
     marginTop: deviceHeight/200,
     fontSize: 12,
-    color: '#ff0066',
+    // color: '#ff0066',
+    color: 'white',
   },
   cam: {
     marginTop: deviceHeight/200,
@@ -306,11 +309,11 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     flex:1,
-    width:null,
-    height: null,
+    width: deviceWidth,
+    height: deviceHeight,
     // padding: 30,
     // marginTop: 20,
-    flexDirection: 'column',
+    // flexDirection: 'column',
     justifyContent: 'center'
   },
   circleImage: {
