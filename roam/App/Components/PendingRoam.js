3c84bd287b8dbd2eb24d8979d1db30cad1dced1b
AJ Grande <ajgrande924@gmail.com>
2016-06-10 17:11:12 -0700

    midpoint on the match view map

--- roam/App/Components/PendingRoam.js
+++ roam/App/Components/PendingRoam.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react';
 import {Text, View, Image, TouchableHighlight, ListView} from 'react-native'
 var styles = require('./Helpers/styles');
 
+
 class PendingRoam extends Component {
-    constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {
+      user: props.user,
       address: null,
       time: null,
+      queryType: '',
+      stateChange: props.passedDownStateChange
     };
   }
 
@@ -15,29 +19,47 @@ class PendingRoam extends Component {
     //we will cancel roam from here
     //remove the roam from db
     //take the user back to the 'Time' page
-    this.props.navigator.pop();
+    fetch('http://localhost:3000/cancelRoam', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id: this.state.user.id})
+    })
+    .then((res) => {
+      this.state.stateChange(1);
+      if (res.status === 200) {
+        AlertIOS.alert('deletion successful');
+      } else {
+        AlertIOs.alert('something wrong happened');
+      }
+    })
+    .catch((error) => {
+      console.log('Error handling submit:', error);
+    });
+
   }
 
   render() {
     return (
-      <Image style={styles.backgroundImage}
+      <Image style={stylesFile.backgroundImage}
         source={require('../../imgs/uni.jpg')}>
-        <Text style={styles.title}> ROAM </Text>
+        <Text style={stylesFile.title}> ROAM </Text>
 
           <Text>We will notify you once you are matched</Text>
           <Text>{this.state.address}</Text>
           <Text>Roam starts at {this.state.time}</Text>
           <TouchableHighlight
-            style={styles.button}
+            style={stylesFile.button}
             onPress={this.handleCancel.bind(this)}
             underlayColor="white" >
-              <Text style={styles.buttonText}>Cancel Roam</Text>
+              <Text style={stylesFile.buttonText}>Cancel Roam</Text>
           </TouchableHighlight>
 
       </Image>
     );
   }
 }
-
 
 module.exports = PendingRoam;
